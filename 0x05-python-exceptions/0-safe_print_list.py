22908574a1fbb6444bcbf279e2ff06c156155d98
Soukaina110 <soukaina.elmoquialle@gmail.com>
2024-01-22 12:48:40 -0800

    projet

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,6 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     try:
     while i in is not x:
-        try:
             print(my_list[i], end="")
             i += 1
         except IndexError:
