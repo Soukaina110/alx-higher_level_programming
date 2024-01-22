16f70dfc1051616697b9ddc020ca011053a41c0c
Soukaina110 <soukaina.elmoquialle@gmail.com>
2024-01-22 12:46:59 -0800

    projet

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    num = 0
-    for i in range(x):
+    i = 0
+    try:
+    while i in is not x:
         try:
             print(my_list[i], end="")
-            num += 1
+            i += 1
         except IndexError:
-            break
+        None
     print()
-    Return
+    return i
