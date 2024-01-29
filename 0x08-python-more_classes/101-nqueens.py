4b6cb257442376c3c276446c36c49c9f5dbd942a
Soukaina110 <soukaina.elmoquialle@gmail.com>
2024-01-29 15:16:02 -0800

    projet

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -115,10 +115,7 @@ def recursive_solve(board, row, queens, solutions):
             xout(tmp_board, row, c)
             solutions = recursive_solve(tmp_board, row + 1,
                                         queens + 1, solutions)
-
-    return (solutions)
-
-
+return (solutions)
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
