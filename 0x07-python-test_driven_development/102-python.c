b73f9fff6d84c352b539d2ddd18e4f5a8195ba29
Soukaina110 <soukaina.elmoquialle@gmail.com>
2024-01-31 09:56:09 -0800

    projet

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -13,21 +13,21 @@ void print_python_string(PyObject *p)
 	PyObject *str, *repr;
 
 	(void)repr;
-	printf("[.] string object info\n");
+	Wprintf(L"[.] string object info\n");
 
 	if (strcmp(p->ob_type->tp_name, "str"))
 	{
-		printf("  [ERROR] Invalid String Object\n");
+		wprintf(L"  [ERROR] Invalid String Object\n");
 		return;
 	}
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
+		wprintf(L"  type: compact ascii\n");
 	else
-		printf("  type: compact unicode object\n");
+		wprintf(L"  type: compact unicode object\n");
 
 	repr = PyObject_Repr(p);
 	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
-	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
-	printf("  value: %s\n", PyBytes_AsString(str));
+	wprintf(L"  length: %ld\n", PyUnicode_GET_SIZE(p));
+	wprintf(L"  value: %s\n", PyBytes_AsString(str));
 }
